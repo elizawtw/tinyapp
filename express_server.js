4a3d41f5e74416bda2e81810fed67822cdbadcd8
elizawtw <twew02@gmail.com>
2021-06-07 21:44:46 +0000

    implemented a function to generate random shortURL

--- express_server.js
+++ express_server.js
@@ -9,9 +9,20 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
+function generateRandomString() {
+  let uniqueURL = '';
+  const str = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for (let i = 6; i > 0; i--) {
+    uniqueURL += str[Math.floor(Math.random() * str.length)]
+  }
+  return uniqueURL;
+}
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -29,6 +40,7 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+  // console.log(generateRandomString())
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -36,6 +48,11 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   
   res.render('urls_show', templateVars)
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
